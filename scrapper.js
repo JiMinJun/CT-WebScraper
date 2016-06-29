f5e8d1a56af910fe1018be51cd07d63de1bff0b9
Ji Min (Gloria) Jun <jiminisme@gmail.com>
2016-06-29 11:40:34 -0700

    removed empty lines and console logs

--- scrapper.js
+++ scrapper.js
@@ -1,9 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-
 var books = [];
-
 request('https://origin-web-scraping.herokuapp.com/', function(error, response, body) {
     if (!error && response.statusCode == 200) {
         var HTML = cheerio.load(body);
@@ -17,18 +15,13 @@ request('https://origin-web-scraping.herokuapp.com/', function(error, response, 
         		'imageUrl' : imageUrl,
         		'author' : author,
         		'price' : price
-        	}
-        	
+        	};
         	books.push(book);
-        })
+        });
     }
-    console.log(books);
     fs.writeFile('books.json', JSON.stringify(books, null, 2), function(err) {
-    	if(err) {
-    		console.log(err);
-    	}
-    })
-
-})
+        console.log(err);
+    });
+});
 
 
