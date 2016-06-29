d044e2011dbb1b490f1892da36f3f585522cc16a
Ji Min (Gloria) Jun <jiminisme@gmail.com>
2016-06-29 10:59:12 -0700

    initial

---
+++ scrapper.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+var books = [];
+
+request('https://origin-web-scraping.herokuapp.com/', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var HTML = cheerio.load(body);
+        HTML('.panel').each(function(i, element) {
+        	var name = HTML(this).children().first().text().replace(/(\n)/, '').trim();
+        	var imageUrl = HTML(this).children().eq(+1).children('img').attr('src');
+        	var author = HTML(this).children().children('p').text();
+        	var price = HTML(this).children().children('.green').text();
+        	var book = {
+        		'name' : name,
+        		'imageUrl' : imageUrl,
+        		'author' : author,
+        		'price' : price
+        	}
+        	
+        	books.push(book);
+        })
+    }
+    console.log(books);
+    fs.writeFile('books.json', JSON.stringify(books, null, 2), function(err) {
+    	if(err) {
+    		console.log(err);
+    	}
+    })
+
+})
+
+
